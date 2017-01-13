2854e3ddc29bffcb0cb48a5a17123c29e78277d1
Andreas T. Eggesvik <andreas.t.eggesvik@hotmail.com>
2017-01-13 08:39:49 +0100

    Update race_of_threads.c

--- race_of_threads.c
+++ race_of_threads.c
@@ -1,1 +1,26 @@
 
+#include <pthread.h>
+#include <stdio.h>
+
+
+int i = 0;
+
+int thread_one(){
+	for (int j = 0; k < 1000000; k++){
+		i++;
+	}
+}
+
+int thread_two(){
+  for (int k= 0; k < 1000000; k++){
+    i--;
+  }
+}
+
+
+void main(){
+	pthread_t Thread_one;
+	pthread_create(&Thread_one, NULL, thread_one, NULL);
+  pthread_t Thread_two;
+  pthread_create(&Thread_two, NULL, thread_two, NULL);
+}
